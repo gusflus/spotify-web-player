eb7467ef29bdda5da8d56784d34e31f45e7ba2cf
gusflus <gus.flusser@gmail.com>
2024-02-24 23:51:32 -0800

    working on translating from his crappy documentation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # spotify web player
 
 testing out the spotify web player api and see if i can transfer to mobile
+
+spotify app redirect uri should be set to: http://localhost:3000/auth/callback
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import WebPlayback from "./WebPlayback";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+  }, []);
+
+  return <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>;
 }
 
 export default App;
